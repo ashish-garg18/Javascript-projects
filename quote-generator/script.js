86bd0e0c26c2f5ab56948f9fea39a24076b2bbcb
Ashish Garg <gashish641@gmail.com>
2021-08-16 22:57:24 +0530

    quote generator added

---
+++ quote-generator/script.js
@@ -0,0 +1,71 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Show new quotes
+function newQuote() {
+    loading();
+    // pick random quotes
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    if(!quote.author) {
+        authorText.textContent = "Unkown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get quotes from API 
+async function getQuotes() {
+    loading();
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) { 
+        // Catch error here
+        getQuotes();
+    }
+}
+
+// Tweet Quote 
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners 
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On load
+getQuotes();
